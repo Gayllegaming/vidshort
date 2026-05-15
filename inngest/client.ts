4ab6a426d102985a7c814a76512c97ea7b708fc8
Gayllegaming <79360096+Gayllegaming@users.noreply.github.com>
2026-05-15 11:53:10 +0530

    Update client.ts

--- inngest/client.ts
+++ inngest/client.ts
@@ -1,4 +1,6 @@
 import { Inngest } from "inngest";
 
-// Create a client to send and receive events
-export const inngest = new Inngest({ id: "vidshort" });
+export const inngest = new Inngest({
+  id: "vidshort",
+  eventKey: process.env.INNGEST_EVENT_KEY,
+});
